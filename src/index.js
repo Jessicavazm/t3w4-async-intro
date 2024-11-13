a2026aa4a7063270cc27be88a2046a57fe31a9ee
Jessicavazm <jjessicavaz@gmail.com>
2024-11-13 13:04:04 +1100

    feat: Add Catch and finally blocks to Write file function

--- src/index.js
+++ src/index.js
@@ -27,24 +27,42 @@ Object.keys(defaultEnv).forEach(
 // Give us an idea of what gets stored in the file
 console.log(contentToWrite);
 
-// All the file handling operations will be handled by the fs module
-const fs = require("node:fs");
+// // All the file handling operations will be handled by the fs module
+// const fs = require("node:fs");
 
 // .env file is created where the node process is running
 // Synchronous file write operation
-fs.writeFileSync(".env", contentToWrite);
+// fs.writeFileSync(".env", contentToWrite);
 
 // Asynchronous file write operation
 // fs.writeFile(filepath, fileContents, callback)
-fs.writeFile(".env", contentToWrite, (err) => {
-    if (err) {
-        console.log("Error writing file: ", err);
-    }
-    else {
-        console.log("File has been written");
-    }
+
+// console.log("Before the fs callback");
+
+// fs.writeFile(".env", contentToWrite, (err) => {
+//     if (err) {
+//         console.log("Error writing file: ", err);
+//     }
+//     else {
+//         console.log("File has been written");
+//     }
+// });
+
+// console.log("After the file write operation");
+
+// console.log("File written successfully");
+
+// Promise version of node-fs
+// Import appropriate module
+const fs = require("node:fs/promises");
+
+console.log("Before the promise");
+fs.writeFile(".env", contentToWrite).then(() => {
+    console.log("After the file has been created.");
+    throw new Error("Hey, you got an error")
+}).catch((error) => {
+    console.log("Error encountered:", error.message);
+}).finally(() => {
+    console.log("Finally block");
 });
-
-
-console.log("File written successfully");
-
+console.log("After the promise");
